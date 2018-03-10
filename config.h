95e5bbcb8c5edf7865f6e664efbf586508376f37
Mickael Sergent <mickael.sergent@free.fr>
2018-03-10 19:05:41 +0100

    OSC in High Speed mode (8MHz)

--- config.h
+++ config.h
@@ -22,7 +22,7 @@ extern "C" {
 // Use project enums instead of #define for ON and OFF.
 
 // CONFIG1H
-#pragma config OSC = INTIO67    // Oscillator Selection bits (Internal oscillator block, port function on RA6 and RA7)
+#pragma config OSC = HS         // Oscillator Selection bits (HS oscillator)
 #pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
 #pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
 
